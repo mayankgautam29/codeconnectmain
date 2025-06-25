d0a7d7134b5aec67e37912d4199530badd8286d2
mayankgautam29 <gautam29july@gmail.com>
2025-06-25 12:32:43 +0530

    third commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,12 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-inferrable-types": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/ban-types": "off",
+      "@typescript-eslint/consistent-type-definitions": "off",
       "no-unused-vars": "off",
     },
   },
